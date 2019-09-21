bd392fbb09a17a9eeeb164ae75d3ed593c50cc1a
Florian Chevallier <florian.chevallier@in-tact.fr>
2019-09-21 15:26:20 +0200

    Suppression de l'inutile

--- app/containers/LanguageProvider/index.js
+++ app/containers/LanguageProvider/index.js
@@ -9,20 +9,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
-import { createSelector } from 'reselect';
 import { IntlProvider } from 'react-intl';
 
-import { makeSelectLocale } from './selectors';
-
-const stateSelector = createSelector(
-  makeSelectLocale(),
-  locale => ({
-    locale,
-  }),
-);
-
 export default function LanguageProvider(props) {
-  const { locale } = useSelector(stateSelector);
+  const { locale } = useSelector(state => state.language.locale)
   return (
     <IntlProvider
       locale={locale}
